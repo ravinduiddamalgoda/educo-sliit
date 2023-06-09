98b88c45f2172f52fbe6524e5d301571d5798d97
DarkZeus <48056504+Dark-Zeus@users.noreply.github.com>
2023-06-09 14:03:47 +0530

    Added search icon, fixed some issues of login and register
    Added search icon, fixed some issues of login and register

--- common_pages/navbar.php
+++ common_pages/navbar.php
@@ -9,8 +9,8 @@ echo '
             <a href="games.php"><li class="nav-link">Games</li></a>
             <a href="about_us.php"><li class="nav-link">About Us</li></a>
             <li class="nav-tab space"></li>
-            <li class="nav-tab f-right">search</li>
-            <li class="nav-tab f-right">P</li>
+            <li class="nav-tab f-right"><i style="font-size: 32px" class="material-icons">search</i></li>
+            <a href="profile_page.php"><li style="border:0px;margin:0px;padding:0px;" class="nav-tab f-right"><i style="font-size: 32px" class="material-icons">account_circle</i></li></a>
         </ul>
     </div>
 </div>
