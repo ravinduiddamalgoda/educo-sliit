95c72c9c0557163e10f3714175ea2db907060340
DarkZeus <48056504+Dark-Zeus@users.noreply.github.com>
2023-06-11 06:05:22 +0530

    some changes to navbar

--- common_pages/navbar.php
+++ common_pages/navbar.php
@@ -5,6 +5,7 @@
 echo '
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 <link href="src/css/nav_style.css" rel="stylesheet">
+
 <div class="container-navbar" id="container-navbar">
     <div class="navbarh" id="navbarh"></div>
     <div class="navbar" id="navbar">
@@ -13,10 +14,8 @@ echo '
             <a href="index.php"><li class="nav-link">Home</li></a>
             <a href="games.php"><li class="nav-link">Games</li></a>
             <a href="about_us.php"><li class="nav-link">About Us</li></a>
-            <li class="nav-tab space"></li>
-            <li class="nav-tab f-right"><i style="font-size: 32px" class="material-icons">search</i></li>
+            <li class="nav-tab space"><div class="search"><input class="searchi" type="text" placeholder="Search"></input><i style="font-size: 32px" class="material-icons">search</i></div></li>
             <a href="profile_page.php"><li style="border:0px;margin:0px;padding:0px;" class="nav-tab f-right"><i style="font-size: 32px" class="material-icons">account_circle</i></li></a>
-
         </ul>
     </div>
 </div>
